dc84003fa307a7fbaff2b309e49ad060863a1220
Joshua Moses <62614287+joshuamoses-cyberark@users.noreply.github.com>
2020-06-30 14:18:08 -0500

    Add files via upload

---
+++ conjurcmd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# DAP: Sample code to retrieve secrets using Summon
+#
+
+conjur variable value $1
---
+++ output.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Sample application code using secrets injected into environment variables via Summon
+#
+
+echo "Database Username: $DB_USER"
+echo "Database Password: $DB_PASS"
